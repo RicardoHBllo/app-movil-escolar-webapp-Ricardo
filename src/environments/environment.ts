9a4823d78dcb7e8f6c8e752f5fee3120a65a48ed
RicardoHBllo <111665401+RicardoHBllo@users.noreply.github.com>
2025-12-04 21:42:36 -0600

    Cambio de angularjson budget para subir a render

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,6 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   url_api: "https://ricardohbllo-control-escolar-desit-api.onrender.com"
 };
